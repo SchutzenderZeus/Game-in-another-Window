36b9d1db3c9463c224e5c23c200b780156de9a13
max20 <maximilian.jatzlau@schiller-og.de>
2023-02-01 13:48:33 +0100

    Savn

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
 
+        String[] Skin = new String[7];
+
         int[] Attributes = new int[9];
 
         System.out.println("Hello World");
